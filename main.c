a61cef511796b6a4078379395d4bd58922ecc1c8
Obsidiam <83480497+Obsidiam@users.noreply.github.com>
2021-05-15 14:23:28 -0400

    i/o test client updated

--- main.c
+++ main.c
@@ -6,23 +6,24 @@
 #include "suggest.h"
 
 int main(void) {
+  char input[64] = "";
   struct triple_s *dict = words_create();
-  char word[64] = "";
-  while (scanf("%s", word) == 1) {
-    printf("%s:", word);
-    int dict_index = sss_search(dict, word);
-    if (dict_index == -1) {
-      struct triple_s *suggestions = suggest(dict, word);
-      if (sss_count(suggestions)) {
-        for (int i = 0; i < sss_count(suggestions); ++i) {
-          printf(" %s", sss_get(suggestions, i));
+  while (scanf("%s", input) == 1) {
+    printf("%s:", input);
+    int index = sss_search(dict, input);
+    if (index != -1) {
+      printf(" Word is correct");
+    } else {
+      struct triple_s *output = suggest(dict, input);
+      int n = sss_count(output);
+      if (n) {
+        for (int i = 0; i < n; ++i) {
+          printf(" %s", sss_get(output, i));
         }
       } else {
-        printf(" NONE");
+        printf(" No Such Word Exists");
       }
-      sss_destroy(suggestions);
-    } else {
-      printf(" OK");
+      sss_destroy(output);
     }
     printf("\n");
   }
